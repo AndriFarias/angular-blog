c80cf5670913e7b563c645b4ae2de8e2bcfaaf38
AndriFarias <andrielly.farias21@gmail.com>
2022-11-03 17:59:43 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://www.omegascopio.com.br/wp-content/uploads/2022/09/Genshin-Impact-anime.jpg",
+    "title": "Vai sair anime do jogo Genshin Impact",
+    "description": "É anunciado um anime de Genshin Impact com Ufotable."
+  },
+  {
+    "id": "2",
+    "photo": "https://img.ibxk.com.br/2022/05/25/25084143529020.jpg",
+    "title": "Anime Spy X Family vira febre",
+    "description": "olá"
+  },
+  {
+    "id": "3",
+    "photo": "https://sucodemanga.com.br/wp-content/uploads/2022/07/Solo-Leveling-anime-thumb.jpg",
+    "title": "O webtoon de Solo Leveling vai ganhar anime",
+    "description": "olá"
+  },
+  {
+    "id": "4",
+    "photo": "https://blogdataverna.com.br/wp-content/uploads/2022/01/16091788235fea1ec71522e_1609178823_3x2_md.jpg",
+    "title": "Anime Kimetsu no Yaba ganhará uma nova temporada",
+    "description": "olá"
+  },
+]
